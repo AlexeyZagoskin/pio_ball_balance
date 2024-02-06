247d5a8ba439ce41db1e1a825ffb89370bc54a19
AlekseyZ <zaleks24@yandex.ru>
2024-02-06 19:23:54 +0300

    П_Регулятор

--- src/student.cpp
+++ src/student.cpp
@@ -21,39 +21,19 @@ void controllerTick (Overlord &over)
     float carX = -over.getCarX ();
     float carVel = -over.getCarVel ();
     float motorAngle = over.getMotorTheta ();
-    float motorVel = over.getMotorVel ();
+    float w = over.getMotorVel ();
 
     float w0 = over.getSlider(SliderEnum::prog1) * 1.0 / 1000;
 
-    float e = w0 - motorVel;
+    float Kp = 1;
 
-    float Tm = 6;
-    float T1 = 35;
-    float K = 0.68;
+    float e = w0 - w;
 
-
-    static float I = 0;
-    static constexpr float Ki = 7;
-    static constexpr float Kp = 1;
-    float Kk = T1 / (2 * K * Tm);
-
-
-    float u = I + Kp * e * Kk;
-
-    if (-10 < u && u < 10 || I * e < 0)
-    {
-        float dI = over.getTs() * Ki * e * Kk;
-        I = I + dI;
-    }
-
-    u = constrain(u, -10, 10);
-    
-
-
+    float u = Kp * e;
 
     Serial.print(w0);
     Serial.print(' ');
-    Serial.println(motorVel);
+    Serial.println(w);
     
     over.setMotorU (u);
 }
