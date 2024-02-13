f02365b26db46be0c7c1a96b6715bcbfbbefab24
AlekseyZ <zaleks24@yandex.ru>
2024-02-13 19:58:22 +0300

    Снятие переходной характеристики

--- src/student.cpp
+++ src/student.cpp
@@ -11,6 +11,18 @@ void controllerInit (Overlord &over)
     over.setSlider (SliderEnum::prog2, -10000, 10000);
 }
 
+float step(uint32_t time, uint32_t period, float min_out, float max_out)
+{
+    if(time/period % 2)
+    {
+        return min_out;
+    }
+    return max_out;
+
+}
+
+
+
 /**
  * @brief Выполнить одну итерацию системы управления
  * @details Вызывается раз в 5мс
@@ -25,21 +37,33 @@ void controllerTick (Overlord &over)
 
     float w0 = over.getSlider(SliderEnum::prog1) * 1.0 / 1000;
 
-    float Ki = 4;
-    float Ts = 0.6;
-    float I = 0; 
+    // static constexpr float Ki = 4, Kp = 1;
+    // static float Ts = 0.6, I = 0;
+    // float Umax = 12;
 
-    float e = w0 - w;
-    float eKi = e * Ki;
-    float dI = eKi * Ts;
-
-    I = I + dI;
-
-    float u = I;
-
-    Serial.print(w0);
-    Serial.print(' ');
-    Serial.println(u);
+    // // float e = w0 - w;
+    // // float eKp = (w0 - w) * Kp;
+    // float u = ((w0 - w) * Kp) + I;
+    // float eKi = ((w0 - w) * Kp) * Ki;
     
+    // uint32_t time = millis();  
+
+    // if(Umax == constrain(u, -12, 12) || I * eKi < 0)
+    // {
+    //     float dI = eKi * Ts;
+    //     I = I + dI;
+
+    // }
+
+    uint32_t time = millis();
+
+    float u = step(time, 2000, 0, 10); // 0 если время не пришло, 10 если время пришло
+
     over.setMotorU (u);
+
+    Serial.print(time);
+    Serial.print(" ");
+    Serial.print(u);
+    Serial.print(" ");
+    Serial.println(w);
 }
